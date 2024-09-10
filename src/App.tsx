f1c2467aa67f4b1f55655a2fdcc92490b9166bf6
benyaasin <yasynaydyn@gmail.com>
2024-09-10 23:48:23 +0300

    lastUpdate

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-expressions */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { useEffect, useState } from "react";
@@ -12,8 +13,8 @@ interface Post {
   "body": string,
 }
 
-async function getPosts() {
-  const response = await fetch(POSTS_URL);
+async function getPosts(page:number=0) {
+  const response = await fetch(POSTS_URL + "?page="+page);
   const postsData: Post[] = await response.json();
   return postsData;
 }
@@ -22,26 +23,44 @@ async function getPosts() {
 function App() {
   const [posts, setPosts] = useState<Post[]>([]);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<any>();
+  const [page, setPage] = useState(0);
 
-
+function handleNextPage(){
+  setPage((oldPage)=>oldPage +1);
+}
 
 
   useEffect(() => {
     (async () => {
       setLoading(true);
-      const postsData = await getPosts();
-      setPosts(postsData);
+      try {
+        const postsData = await getPosts(page);
+        setPosts(postsData);
+      } catch (e: any) {
+        console.log(e);
+        setError(e);
+      }
+
       setLoading(false);
     })();
-  }, []);
+  }, [page]);
 
-  if(loading){
+  if (error) {
+    return (
+
+      <div>Hatalı içerik bulundu , Lütfen tekrar deneyiniz</div>
+    )
+  }
+
+  if (loading) {
     return <div> Yükleniyor...</div>
   }
 
   return (
     <>
       <h1>React Apı Bağlama</h1>
+      <div><button onClick={handleNextPage}>Sonraki sayfa</button></div>
       {posts.map((post) => (
         <li key={post.id}>
           <div style={{ backgroundColor: "gold", color: "black" }}>{post.title}</div>
